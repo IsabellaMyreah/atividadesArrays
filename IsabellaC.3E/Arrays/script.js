e9ae77e976d5e1840136373dc63c625a99946b85
Isabella Cavalcante <108815118+IsabellaMyreah@users.noreply.github.com>
2023-04-10 12:19:41 -0300

    Add files via upload

---
+++ IsabellaC.3E/Arrays/script.js
@@ -0,0 +1,31 @@
+// const users = ["Romário", "Cafú", "Pelé", "Zica"];
+// users.push("Ronaldinho", "Marta", "Cristiano");
+// console.log(users);
+
+// const person = ["Felipe", 30, "São Paulo",  "Masculino", "22233366689"];
+// person.pop();
+// console.log(person);
+
+// const kids = ["nernia", "barbie", "mario", "sonic", "tomEjerry"];
+// kids.shift();
+// console.log(kids);
+
+// const brands = [ "Prada", "Versate", "Gutti"];
+// brands.unshift("Chanel");
+// console.log(brands);
+
+// const jobs = ["Frontend", "Backend", "DevOps", "Fullstack"];
+// let indexOf = jobs.indexOf("Fullstack");
+// console.log(indexOf);
+
+// const fruits = ["Apple", "Orange", "Banana", "Pear"];
+// let indexOfSearch = fruits.indexOf ("Orange", 3);
+// console.log(indexOfSearch);
+
+// const clothes = [ "shorts", "T-shirt", "dress"];
+// clothes.sort();
+// console.log(clothes);
+
+const princess = ["Cinderella", "Aurora", "Bela", "Jasmine"];
+let join = princess.join(" + ");
+console.log(join);
---
+++ IsabellaC.3E/Arrays/script02.js
@@ -0,0 +1,25 @@
+// const colors = ["Red", "Yellow", "Green", "Blue", "White", "Black", "Pink", "Purple"];
+// let join = colors.join(" S2 ");
+// console.log(join);
+
+// const apostles = ["Matheus", "João", "Tiago", "Tomé", "Bartolomeu", "Felipe", "Judas", "Simão", "André", "Tadeu", "Pedro", "Tiago"];
+// apostles.sort();
+
+// console.log(apostles);
+
+// function myFunction (a, b) {
+//     let array = [];
+
+//     if(a.indexOf(b) !== -1) {
+//         array.push(b + a);
+//         return array;
+//     } else {
+//         array.push(a + b);
+//     }
+// }
+
+// console.log(myFunction ("types", "s"));
+
+const narnia = ["Tumnus", "Aslan", "Lucia", "Edmundo", "Susana", "Pedro"]
+narnia.shift();
+console.log(narnia);
