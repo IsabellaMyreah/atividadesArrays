c8d3f5b481dc8ce04b2b12ad79a8769feb8365dd
Isabella Cavalcante <108815118+IsabellaMyreah@users.noreply.github.com>
2023-04-15 11:29:50 -0300

    Add files via upload

---
+++ atividadesArrays/script.js
@@ -0,0 +1,20 @@
+// Escreva um script que remova o último elemento de um array;
+
+const music = ["Gritarra", "Violão", "Microfone", "Bateria", "Pandeiro"];
+music.pop();
+
+console.log(music);
+
+// Escreva um script que adiciona um novo elemento no início do array;
+
+const orgulhoPreconceito = ["Elizabeth", "Jane", "Lydia", "Mary", "Kitty"];
+orgulhoPreconceito.unshift("Darcy");
+
+console.log(orgulhoPreconceito);
+
+// Escreva um script que adiciona um elemento no final do array;
+
+const planets = ["Mercúrio", "Vênus", "Terra", "Marte", "Júpiter", "Saturno", "Urano"];
+planets.push("Netuno");
+
+console.log(planets);
